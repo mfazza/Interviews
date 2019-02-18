3c4133d983dce29ce41d8fb63b32995ffeeecb7f
mattfazza <mathewsfazza@gmail.com>
2019-02-17 23:51:56 -0600

    Renaming

--- src/com/ctc/OnePointOne.java
+++ src/com/ctc/OnePointOne.java
@@ -6,7 +6,7 @@ package com.ctc;
 
 import java.util.*;
 
-public class Main {
+public class OnePointOne {
 
     //O(n^2) and doesn't utilize additional data structures
     public static boolean isUniqueNaive(String str) {
